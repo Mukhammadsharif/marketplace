68838f05de7c4349411879590b7f8650b4b3a31c
Mukhammadsharif <mukhammad683@mail.ru>
2024-04-28 23:07:56 +0500

    add: producers

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,8 +8,23 @@ import Information from "@/components/Information";
 import Products from "@/components/Products";
 import Categories from "@/components/Categories";
 import {CarouselDefault} from "@/components/Carousel";
+import {DOMAIN, PRODUCERS} from "@/helpers/urls";
 
-export default function Home() {
+async function getProducers() {
+    const res = await fetch(`${DOMAIN}${PRODUCERS}`)
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+export default async function Home() {
+  const producers = await getProducers()
   return (
     <MainLayout>
       <Banner />
@@ -18,7 +33,7 @@ export default function Home() {
       <Products />
       <Information />
       <Stats />
-      <Partners />
+      <Partners producers={producers} />
     </MainLayout>
   );
 }
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: '**.com',
-                port: '',
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '8000',
                 pathname: '/**'
             },
         ],
