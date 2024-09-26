21d6b5a19c43d73e4760879b24944dce59f8f806
Mukhammadsharif <mukhammad683@mail.ru>
2024-09-26 11:43:49 +0500

    fix: phone length

--- marketplacebackend/orders/models.py
+++ marketplacebackend/orders/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Order(models.Model):
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=25)
     email = models.EmailField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     order_text = models.TextField()
